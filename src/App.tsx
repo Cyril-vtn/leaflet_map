f476500fe7afd9be6922c59d9be3bed762c087d8
Cyril-SmartV <cyril.votion@smart-video.fr>
2023-12-20 01:30:11 +0100

    header added with button and profile avatar & commented code deleted

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-
+import { Avatar, AvatarBadge, AvatarGroup, Button, Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/react'
 import { useEffect, useState } from 'react';
 import './App.css'
 import { Map } from './components/Map'
@@ -21,40 +21,42 @@ function App() {
   }, []);
 
   return (
-    <div className='bg-gray-100 h-screen w-screen flex flex-col-reverse sm:flex-row'>
-      <div className="bg-white  flex flex-col overflow-auto md:w-96 sm:w-screen ">
-        <h1 className=' text-2xl text-center pt-3 pb-3 shadow-md '>Liste de restaurants</h1>
-
+    <div className='bg-gray-900 h-screen w-screen flex flex-col-reverse sm:flex-row'>
+      <div className="bg-gray-900  flex flex-col overflow-auto md:w-[500px] sm:w-screen ">
+        <h1 className=' text-2xl text-center h-16 pt-3 pb-3 text-white '>Liste de restaurants</h1>
+        {/* small divider */}
+        <div className="border-b-2 border-gray-50 w-60 rounded-lg self-center"> </div>
         <div className='flex flex-col  overflow-auto'>
-
           <SearchList setSelectedRestaurant={setSelectedRestaurant} restaurants={DUMMY_DATA.restaurants} setUserLocation={setUserLocation} setZoom={setZoom} />
-
-          {/* <RestaurantCard restaurant={DUMMY_DATA.restaurants[0]} onClick={() => {
-            setUserLocation(DUMMY_DATA.restaurants[0].location);
-            setZoom(17);
-          }} /> */}
         </div>
-        {/* <div className='flex justify-center h-[15vh]  items-center'>
-          <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full mt-2 mb-2' onClick={() => {
-            navigator.geolocation.getCurrentPosition((position) => {
-              setUserLocation([position.coords.latitude, position.coords.longitude]);
-            });
-            setZoom(13);
-          }}>Se localiser</button>
-        </div> */}
-
-
       </div >
-
-      {/*  */}
-
       {/* Diviseur */}
-      <div className="border-r-2 border-gray-400"></div>
+      <div className="border-r-2 border-gray-50">
+      </div>
+      <div className="flex flex-col w-full h-full">
+        <div className="bg-gray-900 w-full h-[61px] flex flex-row justify-between items-center pr-6 pl-6">
+          <div>
+            <Button size={"sm"} colorScheme="facebook">Ajouter un restaurant</Button>
+          </div>
+          <div className="flex flex-row items-center">
+            <Menu>
+              <MenuButton >
+                <Avatar size={'sm'} cursor={"pointer"}>
+                  <AvatarBadge boxSize="1.25em" bg="green" />
+                </Avatar>
+              </MenuButton>
+              <MenuList zIndex={9999}>
+                <MenuItem>Mon profil</MenuItem>
+                <MenuItem>Se déconnecter</MenuItem>
+              </MenuList>
+            </Menu>
 
-      <div className="bg-gray-200 w-full h-full" >
-
-        <Map restaurants={DUMMY_DATA.restaurants} userLocation={userLocation} zoom={zoom} isUserLocated={isUserLocated} selectedRestaurant={selectedRestaurant} />
-      </div >
+          </div>
+        </div>
+        <div className="bg-gray-900 w-full h-full" >
+          <Map restaurants={DUMMY_DATA.restaurants} userLocation={userLocation} zoom={zoom} isUserLocated={isUserLocated} selectedRestaurant={selectedRestaurant} />
+        </div>
+      </div>
     </div >
   )
 }
