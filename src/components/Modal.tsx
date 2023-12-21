029714270e5a9116a574ca36888082a93ceb10c1
Cyril-SmartV <cyril.votion@smart-video.fr>
2023-12-21 17:13:20 +0100

    form to create account and login & display toast add

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -3,13 +3,17 @@ import {
   Modal as ChakraModal,
   ModalOverlay,
   ModalContent,
-  ModalHeader,
   ModalFooter,
   ModalBody,
-  ModalCloseButton,
   Button,
   Input,
   FormErrorMessage,
+  Tabs,
+  TabList,
+  Tab,
+  TabPanels,
+  TabPanel,
+  useToast,
 } from '@chakra-ui/react';
 
 import {
@@ -18,7 +22,7 @@ import {
   FormHelperText,
 } from '@chakra-ui/react';
 
-import { loginUser } from '../Appwrite/Auth.ts';
+import { createUser, loginUser } from '../Appwrite/Auth.ts';
 
 
 export const Modal = ({
@@ -30,8 +34,8 @@ export const Modal = ({
   setIsOpen: React.Dispatch<React.SetStateAction<boolean>>,
   setUser: React.Dispatch<React.SetStateAction<any>>,
 }) => {
-
-  const  [form, setForm] = useState({
+  const toast = useToast();
+  const [form, setForm] = useState({
     username: '',
     email: '',
     password: '',
@@ -48,7 +52,7 @@ export const Modal = ({
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { id, value } = event.target;
-   setForm({ ...form, [id]: value });
+    setForm({ ...form, [id]: value });
     let errorMessage = '';
     switch (id) {
       case 'username':
@@ -71,17 +75,40 @@ export const Modal = ({
         setForm({ ...form, [id]: value });
         break;
     }
-    
- 
-  };  
 
 
-  const handleForm = async () => {
-    if(!Object.values(errors).some(error => error !== '')) {
+  };
+
+  const handleLogin = async () => {
+    try {
+      await loginUser({ email: form.email, password: form.password });
+      setUser({ username: form.username, email: form.email });
+      setIsOpen(false);
+      createToast('Vous êtes connecté');
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const createToast = (message: string) => {
+    return (
+      toast({
+        title: message,
+        status: 'success',
+        duration: 5000,
+        isClosable: true,
+      })
+    )
+  }
+
+
+  const handleCreateAccount = async () => {
+    if (!Object.values(errors).some(error => error !== '') && Object.values(form).every(value => value !== '') && form.password === form.confirmPassword) {
       try {
-        await loginUser({email: form.email, password: form.password});
-        setUser({username: form.username, email: form.email});
+        await createUser({ email: form.email, password: form.password, name: form.username });
+        setUser({ username: form.username, email: form.email });
         setIsOpen(false);
+        createToast('Votre compte a bien été créé');
       } catch (error) {
         console.log(error);
       }
@@ -90,46 +117,104 @@ export const Modal = ({
 
 
   return (
-    <div>
-      <ChakraModal isOpen={isOpen} onClose={() => setIsOpen(false)}>
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>Se connecter</ModalHeader>
-          <ModalCloseButton />
+      <div>
+        <ChakraModal isOpen={isOpen} onClose={() => setIsOpen(false)}>
+          <ModalOverlay />
+          <ModalContent>
+            <Tabs isFitted colorScheme='blue' variant='enclosed'>
+              <TabList mb='1em'>
+                <Tab fontWeight={'bold'}>Se connecter</Tab>
+                <Tab fontWeight={'bold'}>S'inscrire</Tab>
+              </TabList>
+              <TabPanels>
+                <TabPanel>
+                  <ModalBody>
+                    <FormControl id="email" isRequired mb={5} onChange={handleInputChange} isInvalid={!!errors.email}>
+                      <FormLabel>Email address</FormLabel>
+                      <Input type="email" />
+                      {errors.email && <FormErrorMessage>{errors.email}</FormErrorMessage>}
+                      <FormHelperText>Cette application est une démo, votre email ne sera pas utilisé</FormHelperText>
+                    </FormControl>
+                    <FormControl id="password" isRequired mb={5} onChange={handleInputChange} isInvalid={!!errors.password}>
+                      <FormLabel>Password</FormLabel>
+                      <Input type='password' />
+                      {errors.password && <FormErrorMessage>{errors.password}</FormErrorMessage>}
+                      <FormHelperText>Votre mot de passe doit comporter au moins 8 caractères</FormHelperText>
+                    </FormControl>
+                  </ModalBody>
+                  <ModalFooter>
+                    <Button colorScheme="blue" mr={3} onClick={handleLogin}>
+                      Se connecter
+                    </Button>
+                    <Button variant="ghost" onClick={() => setIsOpen(false)}>Annuler</Button>
+                  </ModalFooter>
+                </TabPanel>
+                <TabPanel>
+                  <ModalBody>
+                    <FormControl id="username" isRequired mb={5} onChange={handleInputChange} isInvalid={!!errors.username}>
+                      <FormLabel>Nom d'utilisateur</FormLabel>
+                      <Input type="text" />
+                      {errors.username && <FormErrorMessage>{errors.username}</FormErrorMessage>}
+                    </FormControl>
+                    <FormControl id="email" isRequired mb={5} onChange={handleInputChange} isInvalid={!!errors.email}>
+                      <FormLabel>Email address</FormLabel>
+                      <Input type="email" />
+                      {errors.email && <FormErrorMessage>{errors.email}</FormErrorMessage>}
+                      <FormHelperText>Cette application est une démo, votre email ne sera pas utilisé</FormHelperText>
+                    </FormControl>
+                    <FormControl id="password" isRequired mb={5} onChange={handleInputChange} isInvalid={!!errors.password}>
+                      <FormLabel>Password</FormLabel>
+                      <Input type='password' />
+                      {errors.password && <FormErrorMessage>{errors.password}</FormErrorMessage>}
+                      <FormHelperText>Votre mot de passe doit comporter au moins 8 caractères</FormHelperText>
+                    </FormControl>
+                    <FormControl id="confirmPassword" isRequired onChange={handleInputChange} isInvalid={!!errors.confirmPassword}>
+                      <FormLabel>Confirmer votre mot de passe</FormLabel>
+                      <Input type='password' />
+                      {errors.confirmPassword && <FormErrorMessage>{errors.confirmPassword}</FormErrorMessage>}
+                    </FormControl>
+                  </ModalBody>
+                  <ModalFooter>
+                    <Button colorScheme="blue" mr={3} onClick={handleCreateAccount}>
+                      S'inscrire
+                    </Button>
+                    <Button variant="ghost" onClick={() => setIsOpen(false)}>Annuler</Button>
+                  </ModalFooter>
+                </TabPanel>
+              </TabPanels>
+            </Tabs>
+          </ModalContent>
           <ModalBody>
-          <FormControl id="username" isRequired mb={5} onChange={handleInputChange} isInvalid={!!errors.username}>
-        <FormLabel>Nom d'utilisateur</FormLabel>
-        <Input type="text" />
-        {errors.username && <FormErrorMessage>{errors.username}</FormErrorMessage>}
-      </FormControl>
-      <FormControl id="email" isRequired mb={5} onChange={handleInputChange} isInvalid={!!errors.email}>
-        <FormLabel>Email address</FormLabel>
-        <Input type="email" />
-        {errors.email && <FormErrorMessage>{errors.email}</FormErrorMessage>}
-        <FormHelperText>Cette application est une démo, votre email ne sera pas utilisé</FormHelperText>
-      </FormControl>
-      <FormControl id="password" isRequired mb={5} onChange={handleInputChange} isInvalid={!!errors.password}>
-        <FormLabel>Password</FormLabel>
-        <Input type='password' />
-        {errors.password && <FormErrorMessage>{errors.password}</FormErrorMessage>}
-        <FormHelperText>Votre mot de passe doit comporter au moins 8 caractères</FormHelperText>
-      </FormControl>
-      <FormControl id="confirmPassword" isRequired onChange={handleInputChange} isInvalid={!!errors.confirmPassword}>
-        <FormLabel>Confirmer votre mot de passe</FormLabel>
-        <Input type='password' />
-        {errors.confirmPassword && <FormErrorMessage>{errors.confirmPassword}</FormErrorMessage>}
-      </FormControl>
+            <FormControl id="username" isRequired mb={5} onChange={handleInputChange} isInvalid={!!errors.username}>
+              <FormLabel>Nom d'utilisateur</FormLabel>
+              <Input type="text" />
+              {errors.username && <FormErrorMessage>{errors.username}</FormErrorMessage>}
+            </FormControl>
+            <FormControl id="email" isRequired mb={5} onChange={handleInputChange} isInvalid={!!errors.email}>
+              <FormLabel>Email address</FormLabel>
+              <Input type="email" />
+              {errors.email && <FormErrorMessage>{errors.email}</FormErrorMessage>}
+              <FormHelperText>Cette application est une démo, votre email ne sera pas utilisé</FormHelperText>
+            </FormControl>
+            <FormControl id="password" isRequired mb={5} onChange={handleInputChange} isInvalid={!!errors.password}>
+              <FormLabel>Password</FormLabel>
+              <Input type='password' />
+              {errors.password && <FormErrorMessage>{errors.password}</FormErrorMessage>}
+              <FormHelperText>Votre mot de passe doit comporter au moins 8 caractères</FormHelperText>
+            </FormControl>
+            <FormControl id="confirmPassword" isRequired onChange={handleInputChange} isInvalid={!!errors.confirmPassword}>
+              <FormLabel>Confirmer votre mot de passe</FormLabel>
+              <Input type='password' />
+              {errors.confirmPassword && <FormErrorMessage>{errors.confirmPassword}</FormErrorMessage>}
+            </FormControl>
           </ModalBody>
           <ModalFooter>
-            <Button colorScheme="blue" mr={3} onClick={handleForm}>
+            <Button colorScheme="blue" mr={3} onClick={handleLogin}>
               Se connecter
             </Button>
             <Button variant="ghost" onClick={() => setIsOpen(false)}>Annuler</Button>
           </ModalFooter>
-        </ModalContent>
-      </ChakraModal>
-
-    </div>
-
+        </ChakraModal>
+      </div>
   )
 }
